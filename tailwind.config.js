98fcdd788f02ddb0ef4473f5e2776cc0b6f92774
Gabriel Aizman <gabaiz88@gmail.com>
2024-05-23 19:46:48 +0200

    version 1.0

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      theme: {
+        colors: {
+          secondary: "#021d34",
+        },
+      },
+      spacing: {
+        597: "597px",
+      },
+    },
+  },
+  plugins: [],
+};
